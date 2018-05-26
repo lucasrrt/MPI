76023401cb7673161faf1d7075da16d4f5d8f0ec
Bruno Vieira Costa <brunovcosta@outlook.com>
2018-05-25 23:46:41 -0300

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 echo "generate matrix"
-sh generate/generate.sh
+cd generate
+sh generate.sh
+cd ..
+
 echo "threads"
+cd thread
 sh thread/thread
+cd ..
+
 echo "mpi"
+cd mpi
 sh mpi/mpi_maquinas.sh
+cd ..
